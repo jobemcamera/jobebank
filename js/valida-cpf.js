68b7d670e486e9aa6d125c893f21460b4819ef29
jobemcamera <jobecamera@gmail.com>
2023-03-05 12:14:33 -0300

    validação dos dígitos do CPF

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,8 +1,10 @@
 export default function CPFValido(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    validaNumerosRepetidos(cpf);
-    console.log(cpf)
-    console.log(validaNumerosRepetidos(cpf))
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        console.log("Esse CPF não existe");
+    } else {
+        console.log("Esse CPF existe");
+    }
 }
 
 function validaNumerosRepetidos(cpf) {
@@ -19,4 +21,40 @@ function validaNumerosRepetidos(cpf) {
         '99999999999'
     ];
     return numeroRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
 }
