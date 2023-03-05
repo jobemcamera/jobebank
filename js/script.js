1233c5ebcea8b17b2034cf0af68d5d145ce24574
jobemcamera <jobecamera@gmail.com>
2023-03-05 11:59:57 -0300

    verificação do CPF: replace e repetição

---
+++ js/script.js
@@ -0,0 +1,13 @@
+import CPFValido from "./valida-cpf.js";
+
+const camposDoFormulario = document.querySelectorAll("[required]");
+
+camposDoFormulario.forEach(campo => {
+    campo.addEventListener("blur", () => verificaCampo(campo));
+});
+
+function verificaCampo(campo) {
+    if (campo.name == "cpf" && campo.value.length >= 11) {
+        CPFValido(campo);
+    }
+}
